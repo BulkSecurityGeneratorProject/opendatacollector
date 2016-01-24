ddaad7b72da1133ad55faf61bee134c49404d8f1
gerardlc <gluqueco@gmail.com>
2016-01-24 12:14:47 +0100

    libreria nvd3 y grafica harcodeadas y funcionando

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,14 @@
 'use strict';
 
 angular.module('openDataCollectorApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Chart, Principal) {
+
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
-        });
+        })
+
+
+
+
     });
